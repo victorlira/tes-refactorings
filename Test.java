178bf11dba86c4cf33e25420aa3dc3be543e2047
antonio <antonio.rec@gmail.com>
2023-10-30 14:46:01 -0300

    base

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test {
+	private String field;
+	
+	public void testing() {
+		field.toUpperCase();
+		field.toLowerCase();
+		field.trimg();
+	}
+	
+	public String getField() {
+		return field;
+	}
+	
+}
